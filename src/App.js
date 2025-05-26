be68dfdf987b640edc390e0fd30a33d2629e8a84
pushkarsingh <tpushkarsingh@gmail.com>
2025-05-26 13:59:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello World from SlayItCoder!</h1>
+      <p>This is your React blog entry point 🚀</p>
     </div>
   );
 }
+
 
 export default App;
